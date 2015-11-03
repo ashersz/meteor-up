5390db8677d238d37fabae1541d4dedd1bbb0e19
ashersz <asher@curiyo.com>
2015-11-03 18:31:20 +0200

    changing the chown to include the group

--- scripts/centos/setup-env.sh
+++ scripts/centos/setup-env.sh
@@ -5,7 +5,7 @@ sudo mkdir -p <%= appRemote %>/<%= appName %>/config
 sudo mkdir -p <%= appRemote %>/<%= appName %>/tmp
 sudo mkdir -p <%= appRemote %>/mongodb
 
-sudo chown ${USER} <%= appRemote %>/<%= appName %> -R
-sudo chown ${USER} <%= appRemote %>/mongodb -R
+sudo chown ${USER}:${USER} <%= appRemote %>/<%= appName %> -R
+sudo chown ${USER}:${USER} <%= appRemote %>/mongodb -R
 
 sudo usermod -a -G docker ${USER}
