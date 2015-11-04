6b16fc2d27bf4131c42f2759d10753d0b341589a
ashersz <asher@curiyo.com>
2015-11-04 12:04:10 +0200

    chown the folders to current user

--- scripts/centos/setup-env.sh
+++ scripts/centos/setup-env.sh
@@ -5,7 +5,7 @@ sudo mkdir -p <%= appRemote %>/<%= appName %>/config
 sudo mkdir -p <%= appRemote %>/<%= appName %>/tmp
 sudo mkdir -p <%= appRemote %>/mongodb
 
-sudo chown ${USER}:${USER} <%= appRemote %>/<%= appName %> -R
-sudo chown ${USER}:${USER} <%= appRemote %>/mongodb -R
+sudo -u ${USER} chown ${USER}:${USER} <%= appRemote %>/<%= appName %> -R
+sudo -u ${USER} chown ${USER}:${USER} <%= appRemote %>/mongodb -R
 
 sudo usermod -a -G docker ${USER}
