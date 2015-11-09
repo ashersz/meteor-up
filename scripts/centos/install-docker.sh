9750f564ceebd523324218a3ccd6d5d7215968cf
ashersz <asher@curiyo.com>
2015-11-09 10:53:49 +0200

    removed echo and useless code

--- scripts/centos/install-docker.sh
+++ scripts/centos/install-docker.sh
@@ -9,19 +9,17 @@ set -e
 if [ ! "$haveDocker" ]; then
 
   # Remove the lock
-  set +e
-  sudo rm /var/lib/dpkg/lock > /dev/null
-  sudo rm /var/cache/apt/archives/lock > /dev/null
-  sudo dpkg --configure -a
-  set -e
-  echo "I am here 2"
+  # does not seem to be needed in centos
+  #set +e
+  #sudo rm /var/lib/dpkg/lock > /dev/null
+  #sudo rm /var/cache/apt/archives/lock > /dev/null
+  #sudo dpkg --configure -a
+  #set -e
+
   # Required to update system
   sudo yum update -y
-  echo "I am here 3"
   # Install docker
-  #wget -qO- https://get.docker.com/ | sudo sh
   curl -sSL https://get.docker.com/ | sudo sh
   sudo service docker start
   sudo chkconfig docker on
-  echo "I am here 4"
 fi
