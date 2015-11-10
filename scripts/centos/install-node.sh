1e38fca7ddc86819d5b9bdf1544250069973f371
ashersz <asher@curiyo.com>
2015-11-10 15:22:54 +0200

    fix nodejs installation

--- scripts/centos/install-node.sh
+++ scripts/centos/install-node.sh
@@ -12,21 +12,12 @@ sudo yum update -y
   NODE_VERSION=0.10.40
 <% } %>
 
-ARCH=$(python -c 'import platform; print platform.architecture()[0]')
-if [[ ${ARCH} == '64bit' ]]; then
-  NODE_ARCH=x64
-else
-  NODE_ARCH=x86
-fi
-
 #sudo yum -y install build-essential libssl-dev git curl
 sudo yum -y install gcc gcc-c++ wget
 
-NODE_DIST=node-v${NODE_VERSION}-linux-${NODE_ARCH}
-
 cd /tmp
-wget http://nodejs.org/dist/v${NODE_VERSION}/${NODE_DIST}.tar.gz
-tar xvzf ${NODE_DIST}.tar.gz && cd node-v*
+wget http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz
+tar xvzf node-v${NODE_VERSION} && cd node-v*
 ./configure
 make
 sudo make install
