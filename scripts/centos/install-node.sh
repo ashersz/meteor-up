6df4282360a47b70b8e8b50958c78e24e4dc5c09
ashersz <asher@curiyo.com>
2015-11-10 16:08:36 +0200

    check if node is already installed

--- scripts/centos/install-node.sh
+++ scripts/centos/install-node.sh
@@ -15,12 +15,18 @@ sudo yum update -y
 #sudo yum -y install build-essential libssl-dev git curl
 sudo yum -y install gcc gcc-c++ wget
 
-cd /tmp
-wget http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz
-tar xvzf node-v${NODE_VERSION}.tar.gz && cd node-v${NODE_VERSION}
-./configure
-make
-sudo make install
-echo `node --version`
+set +e
+haveNode=`node --version`
+set -e
+if [ ! "$haveNode" ]; then
+  cd /tmp
+  wget http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz
+  tar xvzf node-v${NODE_VERSION}.tar.gz && cd node-v${NODE_VERSION}
+  ./configure
+  make
+  sudo make install
+  echo `node --version`
+fi
 #install npm
+sudo yum install epel-release -y
 sudo yum install npm -y
