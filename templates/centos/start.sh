c4bc074fed4606f069705b546520f91988e375cf
ashersz <asher@curiyo.com>
2015-11-10 19:01:09 +0200

    set the env variables for start.sh

--- templates/centos/start.sh
+++ templates/centos/start.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 DIR=<%= appRemote %>/<%= appName %>
 NODE=/usr/local/bin/node
+. ${DIR}/config/env.sh
 PORT=${PORT:-3000}
 test -x $NODE || exit 0
 mkdir -p ~/logs
-touch ~/logs/meteor.log
+LOGFILE=~/logs/meteor.log
+touch ${LOGFILE}
 cd $DIR/app
-env PORT=$PORT MONGO_URL=$MONGO_URL ROOT_URL=$ROOT_URL:$PORT $NODE main.js 1>>"~/logs/meteor.log" 2>&1 &
+env PORT=$PORT MONGO_URL=$MONGO_URL ROOT_URL=$ROOT_URL:$PORT $NODE main.js 1>> ${LOGFILE} 2>&1 &
 echo $! > "$DIR/<%= appName %>.pid"
