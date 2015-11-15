5796a7a893a0d7742eb726838d73c0e6b426d4e7
ashersz <asher@curiyo.com>
2015-11-15 11:02:21 +0200

    use sudo -s

--- templates/centos/init.sh
+++ templates/centos/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # allow appUser to run the appName service
-sudo echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl restart <%= appName %>.service" >> /etc/sudoers
-sudo echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl start <%= appName %>.service" >> /etc/sudoers
-sudo echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl stop <%= appName %>.service" >> /etc/sudoers
+sudo -s
+echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl restart <%= appName %>.service" >> /etc/sudoers
+echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl start <%= appName %>.service" >> /etc/sudoers
+echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl stop <%= appName %>.service" >> /etc/sudoers
+exit
