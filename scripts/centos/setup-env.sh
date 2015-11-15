9f4fdd14b13f31820835b77817e022ecc540eab4
ashersz <asher@curiyo.com>
2015-11-15 11:37:40 +0200

    moved init from deploy to setup

--- scripts/centos/setup-env.sh
+++ scripts/centos/setup-env.sh
@@ -6,8 +6,17 @@ sudo mkdir -p <%= appRemote %>/<%= appName %>/tmp
 sudo useradd -r <%= appUser %>
 
 sudo chown -R ${USER}:${USER} <%= appRemote %>/<%= appName %>
+touch <%= appLog %>/meteor.log
 sudo chown <%= appUser %>:<%= appUser %> <%= appLog %>/meteor.log
 
 # install firewalld
 sudo yum install firewalld -y
+#to do open appPort, if 3000 redirect 80 to 3000, open 27017 or mongo port
 #sudo npm install -g forever userdown wait-for-mongo node-gyp
+
+# allow appUser to run the appName service
+sudo -s
+echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl restart <%= appName %>.service" >> /etc/sudoers
+echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl start <%= appName %>.service" >> /etc/sudoers
+echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl stop <%= appName %>.service" >> /etc/sudoers
+exit
--- templates/centos/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# allow appUser to run the appName service
-sudo -s
-echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl restart <%= appName %>.service" >> /etc/sudoers
-echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl start <%= appName %>.service" >> /etc/sudoers
-echo "<%= appUser %> ALL=(root) NOPASSWD: /usr/bin/systemctl stop <%= appName %>.service" >> /etc/sudoers
-exit
