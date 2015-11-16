c1b5c66d303e06ed8584336411cb973fd291562c
ashersz <asher@curiyo.com>
2015-11-16 13:57:49 +0200

    nodejs install for v0.10.40

--- scripts/centos/install-node.sh
+++ scripts/centos/install-node.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # Required to update system
 sudo yum update -y
 
@@ -12,21 +10,13 @@ sudo yum update -y
   NODE_VERSION=0.10.40
 <% } %>
 
-#sudo yum -y install build-essential libssl-dev git curl
 sudo yum -y install gcc gcc-c++ wget
 
-set +e
-haveNode=`node --version`
-set -e
-if [ ! "$haveNode" ]; then
-  cd /tmp
-  wget http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz
-  tar xvzf node-v${NODE_VERSION}.tar.gz && cd node-v${NODE_VERSION}
-  ./configure
-  make
-  sudo make install
-  echo `node --version`
-fi
-#install npm
+#install npm and nodejs
 sudo yum install epel-release -y
-sudo yum install npm -y
+sudo yum install npm nodejs -y
+#install nvm, make node available for all user under /usr/local/bin/node
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+nvm install v$NODE_VERSION
+nvm alias default v$NODE_VERSION
+n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
